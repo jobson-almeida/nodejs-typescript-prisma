6f620a89906745ded59ff556cde80de6ace9ac83
Jobson Almeida <jobson.ra@outlook.com>
2023-08-04 18:19:15 -0300

    test: should get interests

--- tests/interest/interest.api.test.ts
+++ tests/interest/interest.api.test.ts
@@ -6,8 +6,10 @@ const dataGenerate = (): string => {
   return randomUUID()
 } 
 
+let id = ""
+
 describe("API test using axios", () => {
-  test('Should create an interest from axios', async () => {
+  test('Should create an interest', async () => {
     const response = await axios({
       url: "http://localhost:3000/interests/",
       method: "post",
@@ -22,5 +24,23 @@ describe("API test using axios", () => {
     expect(response.status).toBe(201)
   });
 
+  test('Should get interests', async () => {
+    const response = await axios({
+      url: "http://localhost:3000/interests/",
+      method: "get",
+      responseType: "json",
+      validateStatus: function (status) {
+        return status >= 200 && status < 299;
+      },
+    })
+    const [interest] = response.data
+    id = interest.id
+
+    expect(response).not.toBeNull()
+    expect(response.data).toHaveLength(1)
+    expect(interest.name).toEqual(expect.stringContaining("name"))
+    expect(interest.active).toBeTruthy()
+    expect(response.status).toBe(200)
+  });
 
 });
