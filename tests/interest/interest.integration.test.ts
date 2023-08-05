6922b783a0c7851142b06061627900a644366278
Jobson Almeida <jobson.ra@outlook.com>
2023-08-04 21:31:59 -0300

    it should check, create and list an interest

--- tests/interest/interest.integration.test.ts
+++ tests/interest/interest.integration.test.ts
@@ -1,6 +1,7 @@
+import Interest from "@/domain/entities/interest";
 import InterestRepositoryDatabaseInMemory from "@/infra/repository/memory/interest-repository-in-memory";
 import { randomUUID } from "crypto";
-import { describe, test } from "vitest"
+import { describe, expect, test } from "vitest"
 
 
 const dataGenerate = (): string => {
@@ -15,15 +16,32 @@ let active = true
 
 describe('Integration test', () => {
 
-  test('It should create an interest', async () => {
+  test('It should check, create and list an interest', async () => {
     const input = {
       name: `name ${dataGenerate()}`,
       active: true
     };
 
     const existsInterest = await interestRepository.check({ name: input.name })
-    if (!existsInterest) console.log(existsInterest)
+    let newInterest: Interest
 
+    if (!existsInterest) {
+      newInterest = Interest.create(input.name, input.active)
+      await interestRepository.save(newInterest)
+    }
+    const interestsFound = await interestRepository.list()
+    const [interest] = interestsFound
+    id = interest.id
+    name = interest.name
+    active = false
+
+    expect(existsInterest).toBeFalsy()
+    expect(interestsFound).not.toBeNull()
+    expect(interestsFound).toHaveLength(1)
+    expect(input.name).toBe(interest.name);
+    expect(input.active).toBe(interest.active);
   });
 
+
 });
+
