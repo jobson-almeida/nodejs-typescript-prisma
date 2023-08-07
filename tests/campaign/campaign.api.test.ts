7fa50a63f53c093a7941a3dffc725cdb5cd9c273
Jobson Almeida <jobson.ra@outlook.com>
2023-08-06 22:35:17 -0300

    test: should get campaigns

--- tests/campaign/campaign.api.test.ts
+++ tests/campaign/campaign.api.test.ts
@@ -21,13 +21,34 @@ describe.skip("Campaign api", () => {
     expect(response.data).toHaveLength(1)
     expect(campaign.name).toEqual(expect.stringContaining("name"))
     expect(campaign.text).toEqual(expect.stringContaining("text"))
-    expect(campaign.interests).toEqual(expect.stringContaining("interests"))
+    expect(campaign.campaigns).toEqual(expect.stringContaining("interests"))
     expect(campaign.startTime).toEqual(expect.stringContaining("startTime"))
     expect(campaign.endTime).toEqual(expect.stringContaining("endTime"))
     expect(campaign.active).toBeTruthy()
     expect(response.status).toBe(200)
   })
 
+  test('Should get campaigns', async () => {
+    const response = await axios({
+      url: "http://localhost:3000/campaigns/",
+      method: "get",
+      responseType: "json",
+      validateStatus: function (status) {
+        return status >= 200 && status < 299;
+      },
+    })
+    const [campaign] = response.data
+    id = campaign.id
+
+    expect(response).not.toBeNull()
+    expect(response.data).toHaveLength(1)
+    expect(campaign.name).toEqual(expect.stringContaining("name"))
+    expect(campaign.interests).toEqual(expect.stringContaining("interests"))
+    expect(campaign.startTime).toEqual(expect.stringContaining("startTime"))
+    expect(campaign.endTime).toEqual(expect.stringContaining("endTime"))
+    expect(campaign.active).toBeTruthy()
+    expect(response.status).toBe(200)
+  }); 
   
 });
 
