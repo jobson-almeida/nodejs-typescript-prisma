ea54b32747be49e9125702455060cf5a9954dfa3
Jobson Almeida <jobson.ra@outlook.com>
2023-08-08 20:37:04 -0300

    test: should not create campaign unit without valid name

--- tests/campaign/campaign.unit.test.ts
+++ tests/campaign/campaign.unit.test.ts
@@ -2,24 +2,24 @@ import Campaign from "@/domain/entities/campaign"
 import InvalidObjectError from "@/domain/entities/errors/invalid-object"
 import { randomUUID } from "crypto"
 import { describe, test, expect } from "vitest"
- 
+
 describe("Campaign unit", () => {
-    const now = new Date(Date.now())
-    const numberOfMlSeconds = now.getTime();
-    const addMlSeconds = (1 * 60) * 1000;
-    const after = new Date(numberOfMlSeconds + addMlSeconds);
-  
+  const now = new Date(Date.now())
+  const numberOfMlSeconds = now.getTime();
+  const addMlSeconds = (1 * 60) * 1000;
+  const after = new Date(numberOfMlSeconds + addMlSeconds);
+
   test("Should create campaign unit", () => {
     const input = {
-        name: `name ${randomUUID()}`,
-        text: `text ${randomUUID()}`,
-        interests: [`${randomUUID()}`],
-        startTime: now,
-        endTime: after,
-        status: true,
+      name: `name ${randomUUID()}`,
+      text: `text ${randomUUID()}`,
+      interests: [`${randomUUID()}`],
+      startTime: now,
+      endTime: after,
+      status: true,
     }
-    const campaign = Campaign.create(input.name, input.text, input.interests, input.startTime, input.endTime, input.status) 
-  
+    const campaign = Campaign.create(input.name, input.text, input.interests, input.startTime, input.endTime, input.status)
+
     expect(campaign.name).toEqual(input.name)
     expect(campaign.text).toEqual(input.text)
     expect(campaign.interests).toBeInstanceOf(Array)
@@ -31,6 +31,19 @@ describe("Campaign unit", () => {
     expect(campaign.createdAt).toBeUndefined()
     expect(campaign.updatedAt).toBeUndefined()
   })
+
+  test("Should not create campaign unit without valid name", () => {
+    const input = {
+      name: "",
+      text: `text ${randomUUID()}`,
+      interests: [`${randomUUID()}`],
+      startTime: now,
+      endTime: after,
+      status: true,
+    }
   
+    expect(() => Campaign.create(input.name, input.text, input.interests, input.startTime, input.endTime, input.status)).toThrow(new InvalidObjectError("Invalid name field content: set a name"))    
+  })
+
 
 })
