d1ca71e40d5bc63e60415e007e9d7e3d3a211f1e
Jobson Almeida <jobson.ra@outlook.com>
2023-08-10 20:37:06 -0300

    test: should not create post from empty text

--- tests/post/post.unit.test.ts
+++ tests/post/post.unit.test.ts
@@ -1,3 +1,4 @@
+import InvalidObjectError from "@/domain/entities/errors/invalid-object";
 import Post from "@/domain/entities/post";
 import { randomUUID } from "crypto";
 import { describe, expect, test } from "vitest";
@@ -6,33 +7,42 @@ import { describe, expect, test } from "vitest";
 describe("Post unit", () => {
     let text = ""
     let authorId = ""
-    let post:Post
+    let post: Post
 
     test("Should create post", () => {
         const input = {
-         text: `text ${randomUUID()}`,
-         authorId: `${randomUUID()}`
+            text: `text ${randomUUID()}`,
+            authorId: `${randomUUID()}`
         }
- 
+
         post = Post.create(input.text, input.authorId)
         text = post.text
         authorId = post.authorId
- 
+
         expect(post).not.toBeNull()
         expect(post.id).toBeDefined()
         expect(input.text).toEqual(post.text)
         expect(input.authorId).toEqual(post.authorId)
-     })
+    })
 
-     test("Should update post", () => {
+    test("Should update post", () => {
         const input = {
-         text: `text ${randomUUID()}`,
-         authorId: `${randomUUID()}`
+            text: `text ${randomUUID()}`,
+            authorId: `${randomUUID()}`
         }
- 
-        post.build(input.text, input.authorId)        
+
+        post.build(input.text, input.authorId)
         expect(input.text).not.toEqual(text)
-        expect(input.authorId).not.toEqual(authorId) 
-     })
+        expect(input.authorId).not.toEqual(authorId)
+    })
+
+    test("Should not create post from empty text", () => {
+        const input = {
+            text: "",
+            authorId: `${randomUUID()}`
+        }
+
+        expect(() => Post.create(input.text, input.authorId)).toThrow(new InvalidObjectError("Invalid text field content: set a text"))
+    })
 
 })
