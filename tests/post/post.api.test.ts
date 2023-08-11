a95cc50d964903ed08cb1d19ebc0c20f17bc491c
Jobson Almeida <jobson.ra@outlook.com>
2023-08-11 09:04:16 -0300

    test: should create an post

---
+++ tests/post/post.api.test.ts
@@ -0,0 +1,87 @@
+import axios from "axios"
+import { randomUUID } from "crypto"
+import { afterAll, beforeAll, describe, expect, test } from "vitest"
+
+describe("API test using axios", () => {
+    let id = ""
+    let text = ""
+    let authorId = ""
+    let idInterest = ""
+    let idUser = ""
+
+    beforeAll(async () => {
+        await axios({
+            url: "http://localhost:3000/interests/",
+            method: "post",
+            responseType: "json",
+            data: {
+                name: `name ${randomUUID()}`,
+                active: true
+            },
+            validateStatus: function (status) {
+                return status >= 200 && status < 299;
+            },
+        })
+
+        const responseInterest = await axios({
+            url: "http://localhost:3000/interests/",
+            method: "get",
+            responseType: "json",
+            validateStatus: function (status) {
+                return status >= 200 && status < 299;
+            },
+        })
+        const [interest] = responseInterest.data
+        if (interest) {
+            idInterest = interest.id
+        }
+
+        //------------
+
+        await axios({
+            url: "http://localhost:3000/users/",
+            method: "post",
+            responseType: "json",
+            data: {
+                name: `name ${randomUUID()}`,
+                email: "user@email.com",
+                interests: [`${idInterest}`]
+            },
+            validateStatus: function (status) {
+                return status >= 200 && status < 299;
+            },
+        })
+
+        const responseUser = await axios({
+            url: "http://localhost:3000/users/",
+            method: "get",
+            responseType: "json",
+            validateStatus: function (status) {
+                return status >= 200 && status < 299;
+            },
+        })
+        const [user] = responseUser.data
+        if (user) {
+            idUser = user.id
+        }
+    })
+
+    //---------
+    test('Should create an post', async () => {
+        const response = await axios({
+            url: "http://localhost:3000/posts/",
+            method: "post",
+            data: {
+                text: `text ${randomUUID()}`,
+                authorId: idUser
+            },
+            validateStatus: function (status) {
+                return status >= 200 && status < 299;
+            },
+        })
+        expect(response.status).toBe(201)
+    });
+
+
+
+})
